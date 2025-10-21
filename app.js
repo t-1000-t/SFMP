e0109676c28a164629d1554c8620010739f9e292
vl <goognya@gmail.com>
2025-10-21 16:59:47 +0100

    Ive made a separate the script content to sfpm.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.static(__dirname));
+app.use("/src", express.static(path.join(__dirname, "src")));
+
 app.use("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
