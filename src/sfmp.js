7176e82b0e66cbc1ce2869154585ac548ff6c0ed
vl <goognya@gmail.com>
2025-10-22 14:28:49 +0100

    Ive done the point C of the lesson 3

--- src/sfmp.js
+++ src/sfmp.js
@@ -18,37 +18,54 @@ window.addEventListener("mousemove", (e) => {
   mouse.y = e.clientY - rect.top;
 });
 
+// point B
 // start position center
-let x = window.innerWidth / 2;
-let y = window.innerHeight / 2;
+// let x = window.innerWidth / 2;
+// let y = window.innerHeight / 2;
+// const trail = Array.from({ length: 10 }, () => ({ x: 0, y: 0 }));
 
-const trail = Array.from({ length: 10 }, () => ({ x: 0, y: 0 }));
+// point C
+let angle = 0;
 
 const draw = () => {
   // fade trail
   ctx.fillStyle = "rgba(0,0,0,0.15)";
   ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 
-  // move smoothly toward mouse
-  // x += (mouse.x - x) * 0.03; // slower
-  x += (mouse.x - x) * 0.2; // faster
-  y += (mouse.y - y) * 0.08;
+  // point C
+  angle += 0.05;
+  const orbitRadius = 80;
+  const x = mouse.x + Math.cos(angle) * orbitRadius;
+  const y = mouse.y + Math.sin(angle) * orbitRadius;
 
-  // draw follower
   ctx.beginPath();
   ctx.arc(x, y, 10, 0, Math.PI * 2);
-  ctx.fillStyle = "#5af";
-  ctx.shadowBlur = 20;
-  ctx.shadowColor = "#5af";
+  ctx.fillStyle = "#6f9";
   ctx.fill();
 
+  // point B
+  // move smoothly toward mouse
+  // x += (mouse.x - x) * 0.03; // slower
+  // x += (mouse.x - x) * 0.2; // faster
+  // y += (mouse.y - y) * 0.08;
+
+  //point B
+  // draw follower
+  // ctx.beginPath();
+  // ctx.arc(x, y, 10, 0, Math.PI * 2);
+  // ctx.fillStyle = "#5af";
+  // ctx.shadowBlur = 20;
+  // ctx.shadowColor = "#5af";
+  // ctx.fill();
+
+  // point B
   // a smooth comet tail
-  trail.forEach((p, i) => {
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, 8 - i * 0.5, 0, Math.PI * 2);
-    ctx.fillStyle = `hsl(${200 + i * 10}, 80%, 60%)`;
-    ctx.fill();
-  });
+  // trail.forEach((p, i) => {
+  //   ctx.beginPath();
+  //   ctx.arc(p.x, p.y, 8 - i * 0.5, 0, Math.PI * 2);
+  //   ctx.fillStyle = `hsl(${200 + i * 10}, 80%, 60%)`;
+  //   ctx.fill();
+  // });
 
   requestAnimationFrame(draw);
 };
